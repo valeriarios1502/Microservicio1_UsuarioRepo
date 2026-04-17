756a7cd5f1e6ef0e17cd9ac88ed494f58a13b12d
valeriarios1502 <valeria.rios@utec.edu.pe>
2026-04-17 02:03:16 -0500

    Variables de Entorno

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL",  "postgresql+psycopg2://user:password@3.87.243.107:5432/usuarios_db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(
     DATABASE_URL,
     connect_args={"connect_timeout": 5}
